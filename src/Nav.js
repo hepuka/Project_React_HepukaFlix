0e094a6b96d0a296d4b1c0c73b8e2577af275ff7
Kun-Fagyal Zoltan <kunzoltan7707@gmail.com>
2023-04-02 14:52:10 +0200

    upd

--- src/Nav.js
+++ src/Nav.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from "react";
 import "./Nav.css";
 import avatar from "./img/avatar.jpg";
+import nlogo from "../src/img/nlogo.png";
+
 const Nav = () => {
   const [show, handleShow] = useState(false);
 
@@ -23,11 +25,7 @@ const Nav = () => {
   return (
     <div className={`nav ${show && "nav__black"}`}>
       <div className="nav__contents">
-        <img
-          className="nav__logo"
-          src="https://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png"
-          alt="navlogo"
-        />
+        <img className="nav__logo" src={nlogo} alt="navlogo" />
         <img className="nav__avatar" src={avatar} alt="netflixavatar" />
       </div>
     </div>
